80f63ac73ea08915ba04af82556fd1c0ea6684cf
@rikatussei <yohei.arisu@Gmail.com>
2024-05-29 23:16:52 +0900

    問題１正解

--- practice1.rb
+++ practice1.rb
@@ -1,15 +1,23 @@
-puts "不思議の国"
-puts "Ruby"
-puts "少しずつ学習を進めていきます。"
+# puts "不思議の国"
+# puts "Ruby"
+# puts "少しずつ学習を進めていきます。"
 
-name: 名前
-language: 言語
-hitokoto: 一言
+# name: 名前
+# language: 言語
+# hitokoto: 一言
 
-name = "有須"
-language = "日本語"
-hitokoto = "頑張ります！"
+# name = "有須"
+# language = "日本語"
+# hitokoto = "頑張ります！"
 
-puts "#{name}"
-puts "#{language}"
-puts "#{hitokoto}"
+# puts "#{name}"
+# puts "#{language}"
+# puts "#{hitokoto}"
+
+name = "Kirishima"
+language = "Ruby, English"
+hitokoto = "がんばります!"
+
+puts "name: #{name}"
+puts "language: #{language}"
+puts "hitokoto: #{hitokoto}"
