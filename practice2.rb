5ef0905ffe7bf6bd9c1c06c9e7dce43275440330
@rikatussei <yohei.arisu@Gmail.com>
2024-05-30 19:57:44 +0900

    解答例１

--- practice2.rb
+++ practice2.rb
@@ -1,11 +1,4 @@
-# puts "abc"
-# puts "a"
-# puts 2000
-# puts "0x04a12bE"
-
-# s = gets.chomp
-# # 読み込んだ文字列をそのまま出力する
-# puts s
-
-S = gets.chomp
-puts S
+ cal = 540
+ n = gets.to_i
+if 1 ≦ n ≦ 10
+puts n*cal
