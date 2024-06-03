bc0af4e1926e5de4b6aa3bc79e7eaef343457fd3
@rikatussei <yohei.arisu@Gmail.com>
2024-06-03 23:19:35 +0900

    正解例を作成

--- 3つのデータの入力.rb
+++ 3つのデータの入力.rb
@@ -1,26 +1,20 @@
 # 正解例
 def main
   # 入力値の取得
-  n = gets.to_i
-  strings = []
-  n.times do
-    strings << gets.chomp
-  end
+  input_str = gets.chomp
+
+  # 文字列の分割
+  strings = input_str.split(" ")
 
   # 条件の確認
-  if n < 1 || n > 10
-    puts "Nは1以上10以下の整数である必要があります。"
-    return
-  end
-
   strings.each do |string|
     if string.length < 1 || string.length > 100
       puts "各文字列の長さは1以上100文字以下である必要があります。"
       return
     end
 
-    if /[^\p{ASCII}\s]/ =~ string
-      puts "各文字列は英小文字または大文字または数字または半角スペースのみで構成されている必要があります。"
+    if /[^\p{ASCII}\d]/ =~ string
+      puts "各文字列は英小文字または大文字または数字のみで構成されている必要があります。"
       return
     end
   end
